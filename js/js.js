8f8587e6937a9e94f7498552ded4101303b3d703
buzz69 <sebastien.masson.pro@gmail.com>
2013-07-08 17:39:33 +0200

    test ajax 2

--- js/js.js
+++ js/js.js
@@ -1,19 +1,13 @@
 function comTest(){
-	/*
-	
+	$('#result').html('Please wait...');
 	$.ajax({
 		type: "POST",
 		contentType: "application/x-www-form-urlencoded;charset=utf-8",
 		url: "http://www.cloudsecuritycam.com/COM/ajax.php",
 		data: "action=HELLO",
+		crossDomain: true, 
 		success: function (rep) {
 			$('#result').html(rep);
 		}
-	});
-	
-	*/
-	
-	$.post('http://www.cloudsecuritycam.com/COM/ajax.php', { action: "HELLO" } , function(data) {
-	  $('#result').html(data);
 	});
 }
