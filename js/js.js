35732514e9b308522ecf6bb20fe0b7dcd4385b3f
administrateur <administrateur@administrateur-desktop>
2013-07-08 23:07:50 +0200

    modifs allow cross domain

--- js/js.js
+++ js/js.js
@@ -2,6 +2,7 @@ $( document ).bind( "mobileinit", function() {
     // Make your jQuery Mobile framework configuration changes here!
 
     $.mobile.allowCrossDomainPages = true;
+    $.support.cors = true;
 });
 
 function comTest(){
