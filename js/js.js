5bfec6f83890a4f195838a0569dcd7ff5a32c2c9
administrateur <administrateur@administrateur-desktop>
2013-07-09 00:28:38 +0200

    crossdomain working !!!

--- js/js.js
+++ js/js.js
@@ -2,11 +2,21 @@ function comTest(){
 	$('#result').html('Please wait...');
 	$.ajax({
 		type       : "GET",
-		url        : "http://www.cloudsecuritycam.com/COM/ajax.php",
-		crossDomain: true,
-		data       : {action : 'HELLO', password : 'cloudsecuritycam'},
+		url        : "http://localhost/COM/ajax.php",
+		contentType: "application/json",  
+        	dataType   : 'jsonp',  
+        	data       : {action: 'HELLO', pwd: 'PASSWORD' },
 		success    : function(response) {
-				$('#result').html(response);
-			}
+			$('#result').html(response.text);
+		},
+		error      : function() {
+		    //console.error("error");
+		    alert('Not working!');                  
+		}
 	});
 }
+
+$( document ).bind( "mobileinit", function() {  
+       // Make your jQuery Mobile framework configuration changes here!  
+       $.mobile.allowCrossDomainPages = true;  
+     }); 
